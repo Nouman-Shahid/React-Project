a4b819852640dab76fb281b9d7c43202adec4333
Numan Shahid <noumanshahid2486@gmail.com>
2024-03-20 00:29:32 +0500

    .

---
+++ React-Vite/src/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Heading = () => {
+    return (
+        <>
+            <h1 style={{ color: 'purple', textAlign: "center" }}>Todo App</h1>
+            <h5 style={{ color: 'red', textAlign: "center" }}>using React-Vite</h5>
+        </>
+    )
+}
+
+export default Heading
