bfd89c223c62a33d92dbbaed21473074a4978dc1
Numan Shahid <noumanshahid2486@gmail.com>
2024-04-11 17:40:29 +0500

    add carousel

--- My-Portfolio/src/Components/Project/Project.jsx
+++ My-Portfolio/src/Components/Project/Project.jsx
@@ -1,93 +1,110 @@
-import React from 'react'
-import styles from './Project.module.css'
+import React, { useState, useEffect } from 'react';
+import styles from './Project.module.css';
+import Carousel from 'react-elastic-carousel';
 
 const Project = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const breakpoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 500, itemsToShow: 2 },
+    { width: 700, itemsToShow: 3 },
+    { width: 1400, itemsToShow: 4 },
+  ];
+
+
+  let carousel = [
+    {
+      video: "videos/facebook-clone.mp4",
+      title: 'Facebook Clone',
+      desc: `Built a ReactJS Facebook clone app with user profile generation and article fetching APIs.`,
+      date: 'Posted this . Apr 2024'
+    },
+    {
+      video: "videos/weather.mp4",
+      title: 'Weather App',
+      desc: `I made a weather app with JavaScript that gets its info from an API. It gives pretty good predictions`,
+      date: 'Posted this . Mar 2024'
+    },
+    {
+      video: "videos/yt.mp4",
+      title: 'Youtube Clone',
+      desc: 'I am proud to present my YouTube clone website, built with PHP, SQL, HTML, and CSS.',
+      date: 'Posted this . Jul 2023'
+    },
+    {
+      video: "videos/bullet.mp4",
+      title: 'Bullet Hop',
+      desc: `I've developed a 2D web-based game called 'Bullet Hop' using a combination of JavaScript, HTML and CSS.`,
+      date: 'Posted this . Aug 2023'
+    },
+    {
+      video: "videos/sword.mp4",
+      title: 'Sword & Shield',
+      desc: `I've developed a web-based game called 'Sword & Shield.' I used React.js to build it.`,
+      date: 'Posted this . Aug 2023'
+    },
+    {
+      video: "videos/zombie.mp4",
+      title: 'Zombie Slayer',
+      desc: `I made a 2D game called 'Zombie Slayer.' In the game, you need to protect your character.I used React.js to create it.`,
+      date: 'Posted this . Sep 2023'
+    },
+    {
+      video: "videos/mega-jumps.mp4",
+      title: 'Mega Jumps',
+      desc: `I created a 2D web game called Mega Jumps using React.js. It's inspired by Super Mario, featuring a parallax background, object collisions, and gravity.`,
+      date: 'Posted this . Dec 2023'
+    },
+  ];
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <>
-
-
-
-      <section className={styles.project}>
-
+    <section className={styles.project}>
       <h3 data-aos="fade-up" data-aos-duration="1500"> Testimonial Of  <br /> <span>My Learning</span></h3>
-
-
-
-        <div className={styles.container}>
-
-
-          <div className={styles.intro} data-aos="zoom-in-right" data-aos-duration="2000">
-
-            <h2>Project Variety</h2>
-
-            <p>My portfolio includes diverse projects, such as Android and iOS apps, including calculators, YouTube, Instagram clones, 2D mobile games, and weather and food apps.</p>
-
-            <p>I started making websites and games. I built a YouTube-like site, created games using React.js, and designed a responsive clothing e-commerce website. It's all about learning and getting better at what I do.</p>
-
-            <a href='https://instagram.com/numan.shahid_?igshid=OGQ5ZDc2ODk2ZA=='><button>See all Projects</button></a>
-          </div>
-
-          <img src="images/pic2.gif" className={styles.codingimg} data-aos="zoom-in-left" data-aos-duration="2000"/>
-
+      <div className={styles.container}>
+        <div className={styles.intro} data-aos="zoom-in-right" data-aos-duration="2000">
+          <h2>Project Variety</h2>
+          <p>My portfolio includes diverse projects, such as Android and iOS apps, including calculators, YouTube, Instagram clones, 2D mobile games, and weather and food apps.</p>
+          <p>I started making websites and games. I built a YouTube-like site, created games using React.js, and designed a responsive clothing e-commerce website. It's all about learning and getting better at what I do.</p>
+          <a href='https://instagram.com/numan.shahid_?igshid=OGQ5ZDc2ODk2ZA=='><button>See all Projects</button></a>
         </div>
+        <img src="images/pic2.gif" className={styles.codingimg} data-aos="zoom-in-left" data-aos-duration="2000" />
+      </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4 px-3">
 
-
-
-        <div class="row row-cols-1 row-cols-md-3 g-4 px-4">
-        <div class="col col-md-9 col-lg-5 mx-auto my-3" data-aos="zoom-out" data-aos-duration="1500" >
-            <div class="card h-100  dark-card">
-              <video src="videos/yt.mp4" controls class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Youtube Website Clone</h5>
-                <p class="card-text"> I am proud to present my YouTube clone website, built with PHP, SQL, HTML, and CSS.</p>
+        <Carousel breakpoints={breakpoints} showArrows={false}>
+          {
+            carousel.map((item, index) => (
+              <div key={index} className="col col-md-9 col-lg-5 mx-3 my-3" data-aos="zoom-out" data-aos-duration="1500" style={{ maxHeight: '90vh' }} >
+                <div className="card h-100 dark-card">
+                  <video src={item.video} controls className="card-img-top" style={{ minHeight: '10vh' }} />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    {windowWidth > 560 && <p className="card-text">{item.desc}</p>}
+                  </div>
+                  <div className="card-footer">
+                    <p>{item.date}</p>
+                  </div>
+                </div>
               </div>
-              <div class="card-footer">
-                <p>Posted this . Jul 2023</p>
-              </div>
-            </div>
-          </div>
-          <div class="col col-md-9 col-lg-5 mx-auto my-3" data-aos="zoom-out" data-aos-duration="1500" >
-            <div class="card h-100 dark-card" >
-              <video src="videos/bullet.mp4" controls class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Bullet Hop 2d Game</h5>
-                <p class="card-text">I've developed a 2D web-based game called 'Bullet Hop' using a combination of JavaScript, HTML, and CSS.</p>
-              </div>
-              <div class="card-footer">
-                <p>Posted this . Aug 2023</p>
-              </div>
-            </div>
-          </div>
-          <div class="col col-md-9 col-lg-5 mx-auto my-3" data-aos="zoom-out" data-aos-duration="1500" >
-            <div class="card h-100  dark-card">
-              <video src="videos/sword.mp4" controls class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Sword & Shield 2d Game</h5>
-                <p class="card-text">I've developed a web-based game called 'Sword & Shield.' It's a 2D adventure where you control a character to defeat enemies.I used React.js to build it.</p>
-              </div>
-              <div class="card-footer">
-                <p>Posted this . Aug 2023</p>
-              </div>
-            </div>
-          </div>
-          <div class="col col-md-9 col-lg-5 mx-auto my-3" data-aos="zoom-out" data-aos-duration="1500" >
-            <div class="card h-100  dark-card">
-              <video src="videos/zombie.mp4" controls class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Zombie Slayer 2d Game</h5>
-                <p class="card-text">I made a 2D game called 'Zombie Slayer.' In the game, you need to protect your character from zombies. I used React.js to create it.</p>
-              </div>
-              <div class="card-footer">
-                <p  >Posted this . Sep 2023</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-      </section>
-    </>
-  )
+            ))
+          }
+        </Carousel>
+      </div>
+    </section >
+  );
 }
 
-export default Project
+export default Project;
