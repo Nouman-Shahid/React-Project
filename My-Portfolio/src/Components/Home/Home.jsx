5e74052d78ac3751cbb6dafd7f5bdf19fdbcbe9d
Nouman-Shahid <noumanshahid2486@gmail.com>
2024-05-03 10:22:40 +0500

    code refactoring

--- My-Portfolio/src/Components/Home/Home.jsx
+++ My-Portfolio/src/Components/Home/Home.jsx
@@ -1,10 +1,10 @@
-import React, { useRef, useState, useEffect } from 'react';
-import styles from './Home.module.css';
-import style from './Nav.module.css'
-import About from '../About/About';
-import Project from '../Project/Project';
-import Blog from '../Blog/Blog';
-import Contact from '../Contact/Contact';
+import React, { useRef, useState, useEffect } from "react";
+import styles from "./Home.module.css";
+import style from "./Nav.module.css";
+import About from "../About/About";
+import Project from "../Project/Project";
+import Blog from "../Blog/Blog";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
   const homeRef = useRef(null);
@@ -13,7 +13,7 @@ const Home = () => {
   const blogRef = useRef(null);
   const contactRef = useRef(null);
 
-  const [activeLink, setActiveLink] = useState(null); 
+  const [activeLink, setActiveLink] = useState(null);
 
   function showmenu() {
     var x = document.getElementById("ul");
@@ -29,7 +29,6 @@ const Home = () => {
     setActiveLink(link);
   }
 
-  // Scroll to the 'Home' section when the page is first loaded
   useEffect(() => {
     homeRef.current.scrollIntoView({ behavior: "smooth" });
   }, []);
@@ -38,38 +37,38 @@ const Home = () => {
     <>
       <nav>
         <div className={style.name}>
-          <a href='/'>
+          <a href="/">
             <label> Numan</label>
           </a>
         </div>
-        <ul id='ul'>
+        <ul id="ul">
           <li
-            onClick={() => scrollToSection(homeRef, 'Home')}
-            className={activeLink === 'Home' ? style.activeLink : ''}
+            onClick={() => scrollToSection(homeRef, "Home")}
+            className={activeLink === "Home" ? style.activeLink : ""}
           >
             Home
           </li>
           <li
-            onClick={() => scrollToSection(aboutRef, 'About')}
-            className={activeLink === 'About' ? style.activeLink : ''}
+            onClick={() => scrollToSection(aboutRef, "About")}
+            className={activeLink === "About" ? style.activeLink : ""}
           >
             About
           </li>
           <li
-            onClick={() => scrollToSection(projectsRef, 'Projects')}
-            className={activeLink === 'Projects' ? style.activeLink : ''}
+            onClick={() => scrollToSection(projectsRef, "Projects")}
+            className={activeLink === "Projects" ? style.activeLink : ""}
           >
             Projects
           </li>
           <li
-            onClick={() => scrollToSection(blogRef, 'Blog')}
-            className={activeLink === 'Blog' ? style.activeLink : ''}
+            onClick={() => scrollToSection(blogRef, "Blog")}
+            className={activeLink === "Blog" ? style.activeLink : ""}
           >
             Blog
           </li>
           <li
-            onClick={() => scrollToSection(contactRef, 'Contact')}
-            className={activeLink === 'Contact' ? style.activeLink : ''}
+            onClick={() => scrollToSection(contactRef, "Contact")}
+            className={activeLink === "Contact" ? style.activeLink : ""}
           >
             Contact
           </li>
@@ -80,76 +79,76 @@ const Home = () => {
         </div>
       </nav>
 
-      {/* Home */}
-
       <section ref={homeRef} className={styles.home}>
-
-        <div className={styles.icons} data-aos="fade-right" data-aos-duration="1500">
-         
-         <a href="https://www.facebook.com/nouman.shahid.773?mibextid=D4KYlr"><img src="images/facebook.png" alt="" /></a>
-         <a href="https://instagram.com/numan.shahid_?igshid=OGQ5ZDc2ODk2ZA=="><img src="images/instagram.png" alt="" /></a>
-         <a href="http://www.linkedin.com/in/me-numan"><img src="images/linkedin.png" alt="" /></a>
-         <a href="https://github.com/Nouman-Shahid"><img src="images/github.png" alt="" /></a>
-
+        <div
+          className={styles.icons}
+          data-aos="fade-right"
+          data-aos-duration="1500"
+        >
+          <a href="https://www.facebook.com/nouman.shahid.773?mibextid=D4KYlr">
+            <img src="images/facebook.png" alt="" />
+          </a>
+          <a href="https://instagram.com/numan.shahid_?igshid=OGQ5ZDc2ODk2ZA==">
+            <img src="images/instagram.png" alt="" />
+          </a>
+          <a href="http://www.linkedin.com/in/numan-shahid">
+            <img src="images/linkedin.png" alt="" />
+          </a>
+          <a href="https://github.com/Nouman-Shahid">
+            <img src="images/github.png" alt="" />
+          </a>
         </div>
-        
-        <div className={styles.text} data-aos="fade-right" data-aos-duration="1500">
 
-          <h2>Hello, I am <br /> <br /> <span>Numan Shahid</span></h2>
+        <div
+          className={styles.text}
+          data-aos="fade-right"
+          data-aos-duration="1500"
+        >
+          <h2>
+            Hello, I am <br /> <br /> <span>Numan Shahid</span>
+          </h2>
 
-          <p>"Front-End Developer with <span>Full-Stack</span> Ambitions." 👨‍💻</p>
+          <p>
+            "Front-End Developer with <span>Full-Stack</span> Ambitions." 👨‍💻
+          </p>
 
-          <div className={styles.btns} >
-            <button  onClick={() => scrollToSection(aboutRef, 'About')}
-            className={activeLink === 'About' ? style.activeLink : ''}>About Me</button>
-            <button onClick={() => scrollToSection(contactRef, 'Contact')}
-            className={activeLink === 'Contact' ? style.activeLink : ''}>Hire Me</button>
-           
+          <div className={styles.btns}>
+            <button
+              onClick={() => scrollToSection(aboutRef, "About")}
+              className={activeLink === "About" ? style.activeLink : ""}
+            >
+              About Me
+            </button>
+            <button
+              onClick={() => scrollToSection(contactRef, "Contact")}
+              className={activeLink === "Contact" ? style.activeLink : ""}
+            >
+              Hire Me
+            </button>
           </div>
-
         </div>
 
-        <div className={styles.image} >
-
-      <img src="images/myimage.jpg" alt="" data-aos="fade-left" data-aos-duration="1500" />
-
-
+        <div className={styles.image}>
+          <img
+            src="images/myimage.jpg"
+            alt=""
+            data-aos="fade-left"
+            data-aos-duration="1500"
+          />
         </div>
-
       </section>
-
-      {/* About */}
 
       <section ref={aboutRef} className={styles.about}>
-
         <About />
-
       </section>
-
-      {/* Projects */}
-
 
       <section ref={projectsRef} className={styles.projects}>
-
         <Project />
-
       </section>
-
-
-      {/* Blog */}
-
 
       <section ref={blogRef} className={styles.blog}>
-
         <Blog />
-
       </section>
-
-
-
-      {/* Contact */}
-
-
 
       <section ref={contactRef} className={styles.contact}>
         <Contact />
